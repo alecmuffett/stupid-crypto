596a380658b21d85e810e456f22797a7dcd8e6cd
Ben Laurie <ben@links.org>
2010-02-14 18:58:29 +0000

    Expect failure.

--- test/test.pl
+++ test/test.pl
@@ -20,16 +20,28 @@ for my $test (@tests) {
     my($base) = $test =~ /^(.*)\.stupid$/;
     print "$base...";
     my $code = read_file("$test");
-    if($code =~ /^"EXPECT:([^"]*)"/m) {
-	my $expect = $1;
-	system "./build-$language.sh $base|";
-	open(my $f, "generated/$base |");
-	my $got = read_file($f);
-	close $f;
-	if($expect eq $got) {
-	    print 'OK';
+    if($code =~ /^"EXPECT([^:]*):([^"]*)"/m) {
+	my $expect_status = $1;
+	my $expect_output = $2;
+	if(system("./build-$language.sh $base")) {
+	    if($expect_status eq '-BUILD-FAIL') {
+		print 'OK';
+	    } else {
+		print "BUILD FAIL";
+	    }
 	} else {
-	    print "FAIL (expected '$expect', got '$got')";
+	    if($expect_status eq '') {
+		open(my $f, "generated/$base |");
+		my $got = read_file($f);
+		close $f;
+		if($expect_output eq $got) {
+		    print 'OK';
+		} else {
+		    print "FAIL (expected '$expect_output', got '$got')";
+		}
+	    } else {
+		print "FAIL (expected status $expect_status, got build success)";
+	    }
 	}
     } else {
 	print 'skip';
