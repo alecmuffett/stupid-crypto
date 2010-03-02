db316942822ba942cf910e9b0faa9adc0feed2b3
Ben Clifford <benc@hawaga.org.uk>
2010-03-02 16:29:31 +0100

    Separate out the generated/ subdirectory to one-per-target-language.
    Implement ostreams for Java and use the perl test harness (which now
    passes ok)

---
+++ test/TestWrapper.java
@@ -0,0 +1,10 @@
+
+public class Test__TARGET__ {
+
+public static void main(String args[]) throws java.io.IOException
+    {
+    __TARGET__.test(System.out);
+    System.out.flush();
+    }
+
+}
--- test/build-Haskell.sh
+++ test/build-Haskell.sh
@@ -4,5 +4,5 @@ set -e
 
 TARGET=$1
 
-PERLLIB=../src ../src/stupid.pl --language=Haskell ${TARGET}.stupid > generated/${TARGET}.hs
-ghc -i../lib --make -o generated/${TARGET} generated/${TARGET}.hs 
+PERLLIB=../src ../src/stupid.pl --language=Haskell ${TARGET}.stupid > generated/Haskell/${TARGET}.hs
+ghc -i../lib --make -o generated/Haskell/${TARGET} generated/Haskell/${TARGET}.hs 
